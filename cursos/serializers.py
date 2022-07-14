eac6209fb652d85df00f337c52be089afad2db3d
Filipe <filipeoxs@gmail.com>
2022-07-14 15:56:58 -0300

    Configurando o serializers

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,3 +1,31 @@
 from rest_framework import serializers
-from .models import Base, Avalicacao
+from .models import Curso, Avalicacao
 
+class AvaliacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        #O campo email sera utilizando somente para escrita, assim o email não fica exposto na API
+        extra_kwargs = {
+            'email':{'write_only':True}
+        }
+        model = Avalicacao
+        fields = (
+            'id',
+            'curso',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'publicacao',
+            'ativo'
+        )
+        
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = (
+            'id',
+            'titulo',
+            'url',
+            'publicacao',
+            'ativo'
+        )
